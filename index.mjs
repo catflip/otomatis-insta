fce449c104f4621f6c77f9d3d864dda91f9c3b32
febriano <rinoakbr@gmail.com>
2021-06-10 16:50:05 +0700

    change miscellannous to Miscellaneous

---
+++ index.mjs
@@ -0,0 +1,46 @@
+import { IgApiClient } from 'instagram-private-api';
+import  get  from 'request-promise'; // request is already declared as a dependency of the library
+
+(async () => {
+  try{
+    const ig = new IgApiClient();
+    const clamp = (value, min, max) => Math.max(Math.min(value, max), min);
+    async function generateUsertagFromName(name, x, y)  {
+      // constrain x and y to 0..1 (0 and 1 are not supported)
+      x = clamp(x, 0.0001, 0.9999);
+      y = clamp(y, 0.0001, 0.9999);
+      // get the user_id (pk) for the name
+      const { pk } = await ig.user.searchExact(name);
+      return {
+        user_id: pk,
+        position: [x, y],
+      };
+    }
+    ig.state.generateDevice(process.env.USERNAME);
+    ig.state.proxyUrl = process.env.IG_PROXY;
+    const auth = await ig.account.login(process.env.USERNAME, process.env.PASSWORD);
+    console.log("soklin");
+  
+    // getting random square image from internet as a Buffer
+    const imageBuffer = await get({
+      url: 'https://source.unsplash.com/10x10/?nature', // random picture with 800x800 size
+      encoding: null, // this is required, only this way a Buffer is returned
+    });
+  
+    const publishResult = await ig.publish.photo({
+      file: imageBuffer, // image buffer, you also can specify image from your disk using fs
+      usertags: {
+        in: [
+          // tag the user 'instagram' @ (0.5 | 0.5)
+          await generateUsertagFromName(process.env.USERNAME1, 0.5, 0.5),
+          await generateUsertagFromName(process.env.USERNAME2, 2, 2),
+        ],
+      },
+    });
+  
+    console.log(publishResult.status); // publishResult.status should be "ok"
+  }catch(e){
+console.log(e.message)
+  }
+
+})();
